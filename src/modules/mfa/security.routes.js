d5f9a015cd90e03b79ad797e41feaecd5f658b13
VBharath007 <bababharath005@gmail.com>
2026-04-07 17:09:52 +0530

    approval update

--- src/modules/mfa/security.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const mfaController = require("./mfa.controller");
-
-// POST /api/mfa/verify
-router.post("/verify", mfaController.verifyMfa);
-
-module.exports = router;
